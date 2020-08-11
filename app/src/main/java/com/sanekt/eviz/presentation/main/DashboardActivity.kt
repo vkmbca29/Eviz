f5a566510bff76e8ddd14b6644a4c45ceca03583
radhasharma123 <radhasharma96rs@gmail.com>
2020-08-11 21:46:06 +0530

    "Add Dashboard"

---
+++ app/src/main/java/com/sanekt/eviz/presentation/main/DashboardActivity.kt
@@ -0,0 +1,28 @@
+package com.sanekt.eviz.presentation.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sanekt.eviz.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.action_home, R.id.action_create, R.id.action_account))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
