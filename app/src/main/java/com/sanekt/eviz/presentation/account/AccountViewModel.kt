7d0270f340b471c6825a6ae037ec22dc5d659af6
radhasharma123 <radhasharma96rs@gmail.com>
2020-08-29 15:52:00 +0530

    "Add Create New Card functionality"

--- app/src/main/java/com/sanekt/eviz/presentation/account/AccountViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.sanekt.eviz.presentation.account
-
-import androidx.lifecycle.ViewModel
-
-class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/sanekt/eviz/presentation/create/CreateViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.sanekt.eviz.presentation.create
-
-import androidx.lifecycle.ViewModel
-
-class CreateViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
