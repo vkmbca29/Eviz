9cd6eecf1cdaae37493eccff00b829c1d57e7b4e
vikram <vikram.kumar@philips.com>
2020-08-29 16:37:25 +0530

    dashboard changes

--- app/src/main/java/com/sanekt/eviz/dashboard/DashBoardActivity.kt
+++ app/src/main/java/com/sanekt/eviz/dashboard/DashBoardActivity.kt
@@ -1,6 +1,9 @@
 package com.sanekt.eviz.dashboard
 
 import android.content.Intent
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +18,7 @@ import com.sanekt.eviz.dashboard.model.DashboardModel
 import kotlinx.android.synthetic.main.content_dash_board.*
 
 
-class DashBoardActivity : AppCompatActivity(),MyCardListAdapter.ItemclickListener{
+class DashBoardActivity : AppCompatActivity(), MyCardListAdapter.ItemclickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +32,7 @@ class DashBoardActivity : AppCompatActivity(),MyCardListAdapter.ItemclickListene
             LinearLayoutManager(applicationContext)
         MyCardList.layoutManager = mLayoutManager
         MyCardList.itemAnimator = DefaultItemAnimator()
-        MyCardList.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
+        MyCardList.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         val dashBoardModelList = ArrayList<DashboardModel>()
         dashBoardModelList.add(DashboardModel("titel", "sybtitle", "url", "icon", "daterecieved"))
         dashBoardModelList.add(DashboardModel("titel", "sybtitle", "url", "icon", "daterecieved"))
@@ -42,7 +45,7 @@ class DashBoardActivity : AppCompatActivity(),MyCardListAdapter.ItemclickListene
         dashBoardModelList.add(DashboardModel("titel", "sybtitle", "url", "icon", "daterecieved"))
         dashBoardModelList.add(DashboardModel("titel", "sybtitle", "url", "icon", "daterecieved"))
         dashBoardModelList.add(DashboardModel("titel", "sybtitle", "url", "icon", "daterecieved"))
-        val adapter = MyCardListAdapter(dashBoardModelList,this)
+        val adapter = MyCardListAdapter(dashBoardModelList, this)
         MyCardList.adapter = adapter
 
 
@@ -50,25 +53,38 @@ class DashBoardActivity : AppCompatActivity(),MyCardListAdapter.ItemclickListene
 
     override fun onStart() {
         super.onStart()
-        SharedCardButton.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimaryDark))
-        MyCardButton.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimaryDark))
+        SharedCardButton.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        MyCardButton.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        MyCardButton.setTextColor(ContextCompat.getColor(this, R.color.black))
+        SharedCardButton.setTextColor(ContextCompat.getColor(this, R.color.black))
+
         setonClickListner()
     }
 
     private fun setonClickListner() {
         MyCardButton.setOnClickListener {
-            Toast.makeText(this,"card button clicked",Toast.LENGTH_SHORT).show()
-            SharedCardButton.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimaryDark))
-            MyCardButton.setBackgroundColor(ContextCompat.getColor(this,R.color.blue))
+            Toast.makeText(this, "card button clicked", Toast.LENGTH_SHORT).show()
+            SharedCardButton.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.colorPrimaryDark
+                )
+            )
+            SharedCardButton.setTextColor(ContextCompat.getColor(this, R.color.black))
+            MyCardButton.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+            MyCardButton.setTextColor(ContextCompat.getColor(this, R.color.white))
         }
         SharedCardButton.setOnClickListener {
-            Toast.makeText(this,"shared button clicked",Toast.LENGTH_SHORT).show()
-            MyCardButton.setBackgroundColor(ContextCompat.getColor(this,R.color.colorPrimaryDark))
-            SharedCardButton.setBackgroundColor(ContextCompat.getColor(this,R.color.blue))
+            Toast.makeText(this, "shared button clicked", Toast.LENGTH_SHORT).show()
+            MyCardButton.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+            SharedCardButton.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+            SharedCardButton.setTextColor(ContextCompat.getColor(this, R.color.white))
+            MyCardButton.setTextColor(ContextCompat.getColor(this, R.color.black))
+
         }
         create_new.setOnClickListener {
 //            Toast.makeText(this,"create new cards",Toast.LENGTH_SHORT).show()
-            val sendIntent = Intent(this,MainActivity::class.java)
+            val sendIntent = Intent(this, MainActivity::class.java)
             startActivity(sendIntent)
         }
 
