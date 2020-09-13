124b10793d8711d19946b99da2f6799b1ca08c10
vikram <vikram.kumar@philips.com>
2020-09-13 09:00:01 +0530

    login update

--- app/src/main/java/com/sanekt/eviz/login/ui/LoginActivity.kt
+++ app/src/main/java/com/sanekt/eviz/login/ui/LoginActivity.kt
@@ -2,13 +2,26 @@ package com.sanekt.eviz.login.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.sanekt.eviz.R
 import com.sanekt.eviz.dashboard.DashBoardActivity
 import kotlinx.android.synthetic.main.login_button_layout.*
 
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var callbackManager: CallbackManager
+    private var mGoogleSignInClient: GoogleSignInClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -20,11 +33,130 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        facebook_login_button.setOnClickListener {
-            startActivity(Intent(applicationContext,DashBoardActivity::class.java))
-        }
         google_login_button.setOnClickListener {
-            startActivity(Intent(applicationContext,DashBoardActivity::class.java))
+            signIn()
         }
+        // [END onActivityResult]
+
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        // [START build_client]
+        // Build a GoogleSignInClient with the options specified by gso.
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        // [END build_client]
+        AccessToken.getCurrentAccessToken()
+        callbackManager = CallbackManager.Factory.create()
+        facebook_login_button.registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    Log.i("vikram", "onsuccess")
+//                TODO("Not yet implemented")
+                }
+
+                override fun onCancel() {
+                    Log.i("vikram", "onCancel")
+
+//                TODO("Not yet implemented")
+                }
+
+                override fun onError(error: FacebookException?) {
+                    val accessToken = AccessToken.getCurrentAccessToken()
+                    val isLoggedIn = accessToken != null && !accessToken.isExpired
+                    Log.i("vikram", "onError")
+//                TODO("Not yet implemented")
+                }
+
+
+            })
+        LoginManager.getInstance().retrieveLoginStatus(this, object : LoginStatusCallback {
+            override fun onCompleted(accessToken: AccessToken) {
+                Log.i("vikram", "onsuccess")
+                val accessToken = AccessToken.getCurrentAccessToken()
+                val isLoggedIn = accessToken != null && !accessToken.isExpired
+                Log.i("vikram", "onsuccess")
+                // User was previously logged in, can log them in directly here.
+                // If this callback is called, a popup notification appears that says
+                // "Logged in as <User Name>"
+            }
+
+            override fun onFailure() {
+                // No access token could be retrieved for the user
+            }
+
+            override fun onError(exception: Exception) {
+                // An error occurred
+            }
+        })
+
+        // [START on_start_sign_in]
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+    }
+
+    // [END revokeAccess]
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if (account != null) {
+            Toast.makeText(applicationContext, "loggedIn successfully", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(applicationContext, DashBoardActivity::class.java))
+        } else {
+            Toast.makeText(applicationContext, "logged out successfully", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    // [END handleSignInResult]
+    // [START signIn]
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(
+            signInIntent, RC_SIGN_IN
+        )
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(
+                TAG, "signInResult:failed code=" + e.statusCode
+            )
+            updateUI(null)
+        }
+    }
+
+    companion object {
+        const val RC_SIGN_IN = 9001
+        private const val TAG = "SignInActivity"
+
     }
 }
